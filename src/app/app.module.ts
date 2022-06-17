dbab84d79c684e97261b215181f268fbfccba4db
idanstr <strulovich@mail.tau.ac.il>
2022-06-17 21:26:32 +0300

    Initial commit and basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './pages/app-root/app.component';
+import { MainHeaderComponent } from './cmps/main-header/main-header.component';
+import { MapComponent } from './cmps/map/map.component';
+import { SideBarComponent } from './cmps/side-bar/side-bar.component';
+import { MainFooterComponent } from './cmps/main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MapComponent,
+    SideBarComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule
