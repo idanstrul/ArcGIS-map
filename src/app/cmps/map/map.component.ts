db2f2b66a8194da3b80c155f4da7ce3544fec2af
idanstr <strulovich@mail.tau.ac.il>
2022-06-18 18:28:48 +0300

    Initialized map

--- src/app/cmps/map/map.component.ts
+++ src/app/cmps/map/map.component.ts
@@ -1,15 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  OnDestroy,
+} from '@angular/core';// Default import
+import WebMap from "@arcgis/core/WebMap";
+
+// Namespace import
+import * as projection from "@arcgis/core/geometry/projection";
+
+import MapView from '@arcgis/core/views/MapView';
+import Bookmarks from '@arcgis/core/widgets/Bookmarks';
+import Expand from '@arcgis/core/widgets/Expand';
+import esriConfig from '@arcgis/core/config'
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, OnDestroy {
 
   constructor() { }
+  public view: any = null;
 
-  ngOnInit(): void {
+
+  // The <div> where we will place the map
+  @ViewChild('mapViewNode', { static: true }) private mapViewEl!: ElementRef;
+
+  initializeMap(): Promise<any> {
+    const container = this.mapViewEl.nativeElement;
+
+    const webmap = new WebMap({
+      portalItem: {
+        id: 'aa1d3f80270146208328cf66d022e09c ',
+      },
+    });
+
+    const view = new MapView({
+      container,
+      map: webmap
+    });
+
+    const bookmarks = new Bookmarks({
+      view,
+      // allows bookmarks to be added, edited, or deleted
+      editingEnabled: true,
+    });
+
+    const bkExpand = new Expand({
+      view,
+      content: bookmarks,
+      expanded: true,
+    });
+
+    // Add the widget to the top-right corner of the view
+    view.ui.add(bkExpand, 'top-right');
+
+    // bonus - how many bookmarks in the webmap?
+    webmap.when(() => {
+      if (webmap.bookmarks && webmap.bookmarks.length) {
+        console.log('Bookmarks: ', webmap.bookmarks.length);
+      } else {
+        console.log('No bookmarks in this webmap.');
+      }
+    });
+
+    this.view = view;
+    return this.view.when();
+  }
+
+
+  ngOnInit(): any {
+    // Initialize MapView and return an instance of MapView
+    this.initializeMap().then(() => {
+      // The map has been initialized
+      console.log('The map is ready.');
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.view) {
+      // destroy the map view
+      this.view.destroy();
+    }
   }
 
 }
